9070f1fbe3091007c322c4e66ba06439e6baf052
Marc Bouvier <m.bouvier.dev@gmail.com>
2022-05-08 10:22:30 +0200

    refactor: use template

--- talks/list/list.go
+++ talks/list/list.go
@@ -1,15 +1,57 @@
 package list
 
+import (
+	"bytes"
+	"text/template"
+)
+
 func Do() (string, error) {
 	return plainTextFutureTalksList(), nil
 }
 
-func plainTextFutureTalksList() string {
-	return `Marc Bouvier will be speaking in the following events:
+type talkInfos struct {
+	Title                 string
+	EventName             string
+	EventDay              string
+	EventTime             string
+	EventAddressPlaceName string
+	EventAddressStreet    string
+	EventAddressCity      string
+	EventAddressCountry   string
+	EventUrl              string
+}
 
-- Tu ne le sais pas encore mais tu l'as déjà documenté!
-  - Agile tour Strasbourg 2022
-  - 20th may 2022 • 9am - 6pm
-  - Epitech Strasbourg • 4 Rue du Dôme • Strasbourg • France
-  - https://www.meetup.com/MugStrasbourg/events/281350303/`
+func plainTextFutureTalksList() string {
+	var talk = talkInfos{
+		Title:                 "Tu ne le sais pas encore mais tu l'as déjà documenté!",
+		EventName:             "Agile tour Strasbourg 2022",
+		EventDay:              "20th may 2022",
+		EventTime:             "9am - 6pm",
+		EventAddressPlaceName: "Epitech Strasbourg",
+		EventAddressStreet:    "4 Rue du Dôme",
+		EventAddressCity:      "Strasbourg",
+		EventAddressCountry:   "France",
+		EventUrl:              "https://www.meetup.com/MugStrasbourg/events/281350303/",
+	}
+	t, parseErr := template.New("talksList").Parse(
+		`Marc Bouvier will be speaking in the following events:
+
+- {{ .Title}}
+  - {{ .EventName}}
+  - {{ .EventDay}} • {{ .EventTime}}
+  - {{ .EventAddressPlaceName}} • {{ .EventAddressStreet}} • {{ .EventAddressCity}} • {{ .EventAddressCountry}}
+  - {{ .EventUrl}}`)
+
+	if parseErr != nil {
+		panic(parseErr)
+	}
+
+	var tmplBytes bytes.Buffer
+
+	err := t.Execute(&tmplBytes, talk)
+	if err != nil {
+		panic(err)
+	}
+	return tmplBytes.String()
+
 }
